cb5fb18cea123c720a670046ad93bea01e3835c8
f2hard3 <f2hard3@gmail.com>
2019-09-09 21:06:02 +0900

    feat(categories) add category feature

---
+++ pages/categories/_id.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  validate({ params }) {
+    return !isNaN(+params.id)
+  },
+  asyncData({ $axios, params }) {
+    return $axios
+      .get(`http://localhost:3001/recipes?categoryId=${params.id}`)
+      .then((res) => {
+        return { recipes: res.data }
+      })
+  }
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,24 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        vue-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My mind-blowing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <app-category
+      v-for="category in categories"
+      :key="category.id"
+      :category="category"
+    ></app-category>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Category from '../components/Category'
 
 export default {
-  components: {
-    Logo
+  components: { 'app-category': Category },
+  asyncData({ $axios }) {
+    return $axios.get(`http://localhost:3001/categories`).then((res) => ({
+      categories: res.data
+    }))
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
